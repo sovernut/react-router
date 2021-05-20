04117f0ff8f2ae07be71c99bc28d78df4f150f7d
sovernut <orenomail4@gmail.com>
2021-05-20 22:45:24 +0700

    nested Route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
+const Welcome = (props) => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user</p>
+      </Route>
+    </section>
+  );
+};
 
-const Welcome = props => {
-    return (
-        <h1>
-            The Welcome Page
-        </h1>
-    )
-}
-
-export default Welcome
+export default Welcome;
